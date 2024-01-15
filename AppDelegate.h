c961b5467a65086aab4c4eb50cd468d2b9e021dd
PriyankaRently <156666176+PriyankaRently@users.noreply.github.com>
2024-01-15 17:44:56 +0530

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  p1
+//
+//  Created by Rently Coimbatore on 15/01/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
